c2075626afd09375421c47e154a0f937434de7cc
Abhishek <abhishekgorenka@gmail.com>
2024-11-11 13:38:41 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,6 @@ const getTotalIsles = function (grid) {
   let islandcount = 0;
 
   function dfs(grid, row, col) {
-    // If out of bounds or on water, stop the search.
     if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] !== 'L') {
         return;
     }
