ab76385d0cf6d05e338914b5af52816ea5faf14f
Abhishek <abhishekgorenka@gmail.com>
2024-11-11 13:43:54 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,20 +1,20 @@
 const getTotalIsles = function (grid) {
 
-  var islandcount = 0;
-
+  
   function dfs(grid, row, col) {
     if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] !== "L") {
       return;
     }
-
+    
     grid[row][col] = "W";
-
+    
     dfs(grid, row - 1, col);
     dfs(grid, row + 1, col);
     dfs(grid, row, col - 1);
     dfs(grid, row, col + 1);
   }
-
+  
+  let islandcount = 0;
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[0].length; col++) {
       if (grid[row][col] === "L") {
